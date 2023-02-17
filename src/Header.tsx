22ee8aa39aacdccf641536b5b0a0be2df8e8b3ee
ivoff <pyeroevo@gmail.com>
2023-02-16 23:14:14 -0400

    update readme

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,7 @@ import { ReactElement } from "react";
 export default function Header(): ReactElement {
     return (
         <div className="py-6 bg-violet-200">
-            <h1 className="text-4xl text-violet-500 text-center font-mono font-bold">//TODO: App</h1>            
+            <h1 className="text-4xl text-violet-500 text-center font-mono font-bold">{`//TODO: App`}</h1>
         </div>
     ); 
 }
